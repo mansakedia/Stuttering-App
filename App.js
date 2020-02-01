81d2ae51eba6b4254683ca6b701e8e39be9ee576
Mansa <kedia.25@buckeyemail.osu.edu>
2020-02-01 11:13:45 -0500

    Capstone Demo 1

--- App.js
+++ App.js
@@ -1,19 +1,81 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, Text, View, TouchableOpacity, StyleSheet } from 'react-native';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
-}
+import {createAppContainer, createSwitchNavigator} from 'react-navigation';
+import {createBottomTabNavigator, createMaterialTopTabNavigator} from 'react-navigation-tabs';
+import {createStackNavigator} from 'react-navigation-stack';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+import SignInScreen from './Demo/SignInScreen';
+import VerifyIDScreen from './Demo/VerifyIDScreen';
+import ExerciseScreen from './Demo/ExerciseScreen';
+import SocialScreen from './Demo/SocialScreen';
+import AllExercise from './Demo/AllExercise';
+
+const RootStack2 = createMaterialTopTabNavigator({
+  Assigned: { screen: ExerciseScreen },
+  All: { screen: AllExercise },
+})
+
+const RootStack = createBottomTabNavigator({
+    Exercise: { screen: RootStack2 },
+    Social: { screen: SocialScreen },
   },
-});
+  {
+    defaultNavigationOptions: ({ navigation }) => ({
+      tabBarIcon: ({ focused, horizontal, tintColor }) => {
+        const { routeName } = navigation.state;
+        let IconComponent = Ionicons;
+        let iconName;
+        if (routeName === 'Exercise') {
+          iconName = 'md-stopwatch';
+        } else if (routeName === 'Social') {
+          iconName = 'md-people';
+        }
+        // You can return any component that you like here!
+        return <IconComponent name={iconName} size={25} color={tintColor} />;
+      },
+    }),
+    tabBarOptions: {
+      activeTintColor: 'blue',
+      inactiveTintColor: 'gray',
+    },
+  }
+);
+
+// const nextNavigator2 = createSwitchNavigator(
+//     {
+//       Home: RootStack,
+//     },
+//     {
+//       initialRouteName: 'Home',
+//     }
+//   );
+
+//createSwitchNavigator
+const bottomNavigator = createStackNavigator(
+    {
+      SignIn: SignInScreen,
+      VerifyID: {
+        screen: VerifyIDScreen,
+        navigationOptions: {
+          headerLeft: null
+        }
+      },
+      Main: {
+        screen: RootStack,
+        navigationOptions: {
+          headerLeft: null
+        }
+      },
+    },
+    {
+      initialRouteName: 'SignIn',
+    }
+  );
+  const AppContainer = createAppContainer(bottomNavigator);
+  export default class App extends React.Component {
+    render() {
+      return <AppContainer />;
+    }
+  }
